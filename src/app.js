b5e48d3dcf74552f50b412c2a8beb9db9d4f9977
sophieg1864 <sophieg1864@gmail.com>
2025-03-09 08:55:52 +1100

    Added forecast days function

--- src/app.js
+++ src/app.js
@@ -53,6 +53,12 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
 function getForecast(city) {
   let apiKey = "o25c4a6b28aad038t2df43923d980efb";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}}&key=${apiKey}&unit=metric`;
@@ -60,8 +66,6 @@ function getForecast(city) {
   console.log(apiUrl);
 }
 function displayForecast(response) {
-  console.log(response.data);
-
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
@@ -70,7 +74,7 @@ function displayForecast(response) {
         forecastHtml +
         `
       <div class="weather-forecast-day">
-            <div class="weather-forecast-date">Sat</div>
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
             <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
